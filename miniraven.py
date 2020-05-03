7238b82938d57cbd8864899652ec5ed141b8b554
Michael Reim <kraileth@elderlinux.org>
2020-05-03 19:24:47 +0200

    Split out special functions

--- miniraven.py
+++ miniraven.py
@@ -8,13 +8,12 @@ import globalvars
 import modules.conf as conf
 import modules.misc as misc
 import modules.platform as platform
+import modules.special as special
 import modules.subst as subst
 import configparser
 import os
-import re
-import socket
+import shutil
 import subprocess
-import time
 
 ###############
  # Functions #
@@ -186,24 +185,6 @@ def ensure_patchfiles_present(package):
                     misc.die("Mismatch again! Bailing out...")
         i = i + 1
 
-def prepare_bmake_patch():
-    misc.verbose_output("Adapting bmake patch to target system... ")
-    for s in ["OSNAME", "OSVERSION", "OSRELEASE", "OSMAJOR", "OSARCH", "STDARCH"]:
-        misc.reinplace(globalvars.SUBSTITUTION_MAP['rbuild_patches_dir'] + "/bmake/patch-main.c", s, eval("globalvars." + s))
-    misc.verbose_output("ok\n")
-
-def prepare_uname_source():
-    wrkdir = misc.get_wrkdir("uname")
-    misc.verbose_output("Applying plattform info to fake uname... ")
-    misc.reinplace(wrkdir + "/uname.c.in", "\"@OPSYS@\"", globalvars.OSNAME)
-    misc.reinplace(wrkdir + "/uname.c.in", "\"@ARCH@\"", globalvars.OSARCH)
-    misc.reinplace(wrkdir + "/uname.c.in", "\"@PLATFORM@\"", globalvars.STDARCH)
-    misc.reinplace(wrkdir + "/uname.c.in", "\"@RELEASE@\"", globalvars.OSRELEASE + "-RAVEN")
-    misc.reinplace(wrkdir + "/uname.c.in", "\"@USERVER@\"", globalvars.OSVERSION)
-    misc.reinplace(wrkdir + "/uname.c.in", "\"@OPSYS@ @RELEASE@ #0 Sat Jul 29 09:00:00 CDT 2017 root@octavia.unreal.systems:/usr/obj/usr/src/sys/GENERIC\"", globalvars.OSNAME + " " + globalvars.OSRELEASE + " #0 " + time.ctime() + " root@" + socket.getfqdn() + ":/usr/obj/usr/src/sys/GENERIC")
-    misc.reinplace(wrkdir + "/uname.c.in", "\"octavia.unreal.systems\"", socket.getfqdn())
-    misc.verbose_output("ok\n")
-
 def build_package(phase, package):
     if phase == "configure":
         activity = "Configuring"
@@ -230,11 +211,11 @@ def build_missing():
     for p in packages_missing:
         ensure_clean_wrkdir(p)
         if p == "uname":
-            prepare_uname_source()
+            special.prepare_uname_source()
         if p in conf.config['patches']:
             ensure_patchfiles_present(p)
             if p == "bmake":
-                prepare_bmake_patch()
+                special.prepare_bmake_patch()
             misc.patch_source(p)
         if p in conf.config['configure_cmds']:
             build_package('configure', p)
